e79a99f9ce388840072fd7a52b85964e727b7368
m.ayank.r <red.mayank.r@gmail.com>
2022-10-16 15:20:01 +0530

    4A

---
+++ CodeForces/.driverCode.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define int long long int
+#define cin(dt) *istream_iterator<dt>(cin) 
+using namespace std;
+
+void tc(){
+    
+}
+
+signed main(){
+
+    int t;
+    cin>>t;
+    while(t--)
+        tc();
+
+    return 0;
+}
---
+++ CodeForces/.practice/4A_Watermelon/4A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define int long long int
+#define cin(dt) *istream_iterator<dt>(cin) 
+using namespace std;
+
+void tc(){
+    
+}
+
+signed main(){
+
+    int w = cin(int);
+    if(w == 2)
+        cout<<"NO";
+    else if(w%2 == 0)
+        cout<<"YES";
+    else
+        cout<<"NO";
+
+    return 0;
+}
