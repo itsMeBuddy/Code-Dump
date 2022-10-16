c6de1770aa3c438a23c4926aac4bc30243f6c21c
m.ayank.r <red.mayank.r@gmail.com>
2022-10-16 15:40:22 +0530

    231A

--- CodeForces/.practice/231A_Team/231A.cpp
+++ CodeForces/.practice/231A_Team/231A.cpp
@@ -4,13 +4,21 @@
 using namespace std;
 
 void tc(){
-    
+    int n;
+    cin>>n;
+    int cnt = 0;
+    while(n--){
+        int sum = cin(int) + cin(int) + cin(int);
+        if(sum >= 2)
+            cnt++;
+    }
+    cout<<cnt;
 }
 
 signed main(){
 
     int t = 1;
-    cin>>t;
+    // cin>>t;
     while(t--)
         tc();
 
